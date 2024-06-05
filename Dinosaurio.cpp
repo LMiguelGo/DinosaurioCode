9ac5a07782899019802ff2c0e3a013f29ff730e9
LMiguelGo <luismigomez@unicauca.edu.co>
2024-06-05 01:48:39 -0500

    Dinosaurio.cpp

---
+++ Dinosaurio.cpp
@@ -0,0 +1,30 @@
+#include "Dinosaurio.h"
+
+Dinosaurio::Dinosaurio() {
+  estado = 0;  
+  y = 0;   
+  longSalto = 2;
+}
+
+void Dinosaurio::caminar() {    
+    y = 0;
+    if(estado == 1) {
+      estado = 2;
+    } 
+    else {
+      estado = 1;
+    } 
+}
+
+void Dinosaurio::saltar() {
+  y = 1;
+  estado = 3;
+}
+
+int Dinosaurio::obtenerEstado() {
+  return estado;
+}
+
+int Dinosaurio::obtenerPosicion() {
+  return y;
+}
